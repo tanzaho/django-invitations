aff7b9cdc44c78c8f7a92fc4edc06a52203f5158
Jean-Christophe Lavocat <jice@lavocat.name>
2016-08-03 18:51:55 +0200

    Sending html templates

--- invitations/tasks.py
+++ invitations/tasks.py
@@ -2,6 +2,7 @@
 from django_rq import job, enqueue, get_queue, get_scheduler
 from django.conf import settings
 from django.template import loader, Context
+from django.template.loader import render_to_string
 from django.core.mail.message import EmailMessage
 from datetime import timedelta
 import logging
@@ -50,15 +51,16 @@ def send_reminder(email, newsletter_name, title, ctx, invitation_id):
 
     html_content = nl_folder + ".txt"
     # select the template by default first
-    tpl_html = loader.select_template([html_content])
+    # tpl_html = loader.select_template([html_content])
 
     # Rendering
     context = Context(ctx)
-    html_content = tpl_html.render(context)
+    # html_content = tpl_html.render(context)
+    html_content = render_to_string(html_content, context).strip()
 
     # Sending email
     email = EmailMessage(subject=title, body=html_content, from_email=settings.JICE_FROM_EMAIL, to=[email], bcc=["jice@lavocat.name"])
-    # email.content_subtype = "text/html"  # Main content is now text/html (or just html?)
+    email.content_subtype = "html"  # Main content is now text/html (or just html?)
     tag = newsletter_name
     email.tags = [tag]
     email.track_clicks = True
