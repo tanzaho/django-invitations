e34ec9e884a4b6480f2ea8461e61b6632cf324b3
Jean-Christophe Lavocat <jice@lavocat.name>
2016-10-14 12:38:46 +0200

    New migration scheme for Postgre compatibility

--- invitations/migrations/0006_auto_20161014_1137.py
+++ invitations/migrations/0006_auto_20161014_1137.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import invitations.models
-from invitations.models import Invitation, _create_unsubscribe_slug
 
 
 class Migration(migrations.Migration):
@@ -12,25 +11,17 @@ class Migration(migrations.Migration):
         ('invitations', '0005_auto_20160725_1814'),
     ]
 
-    def set_unsub_slug(apps, schema_editor):
-        for row in Invitation.objects.all():
-            row.unsubscribe_slug = _create_unsubscribe_slug()
-            row.save()
-
     operations = [
-
         migrations.AddField(
             model_name='invitation',
             name='unsubscribe_slug',
             field=models.SlugField(default=invitations.models._create_unsubscribe_slug, max_length=32),
             preserve_default=True,
         ),
-
-        migrations.RunPython(set_unsub_slug),
-
-        migrations.AlterField(
+        migrations.AddField(
             model_name='invitation',
-            name='unsubscribe_slug',
-            field=models.SlugField(default=invitations.models._create_unsubscribe_slug, unique=True, max_length=32),
+            name='want_mails',
+            field=models.BooleanField(default=True),
+            preserve_default=True,
         ),
     ]
