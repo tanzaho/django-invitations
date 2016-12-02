3f04f7ce9f11f64b6133489d71749276613e9b21
Jean-Christophe Lavocat <jice@lavocat.name>
2016-12-02 10:01:35 +0100

    Added an action to turn off emails

--- invitations/admin.py
+++ invitations/admin.py
@@ -6,6 +6,7 @@ from .forms import InvitationAdminAddForm, InvitationAdminChangeForm
 
 class InvitationAdmin(admin.ModelAdmin):
     list_display = ('email', 'sent', 'accepted', 'confirmed', 'want_mails')
+    actions = ['switchReceiveMailTrue', 'switchReceiveMailFalse']
 
     def get_form(self, request, obj=None, **kwargs):
         if obj:
@@ -15,5 +16,22 @@ class InvitationAdmin(admin.ModelAdmin):
             kwargs['form'].user = request.user
             kwargs['form'].request = request
         return super(InvitationAdmin, self).get_form(request, obj, **kwargs)
+
+    def switchReceiveMailTrue(self, request, queryset):
+        for invitation in queryset:
+            if invitation.email != '91decf@gmail.com':
+                invitation.want_mails = True;
+                invitation.save()
+        self.message_user(request, "%s invitations successfully turned to True." % len(queryset))
+    switchReceiveMailTrue.short_description = "Turn 'want mail' to True"
+
+    def switchReceiveMailFalse(self, request, queryset):
+        for invitation in queryset:
+            if invitation.email != '91decf@gmail.com':
+                invitation.want_mails = False;
+                invitation.save()
+        self.message_user(request, "%s invitations successfully turned to False." % len(queryset))
+    switchReceiveMailFalse.short_description = "Turn 'want mail' to False"
+
 
 admin.site.register(Invitation, InvitationAdmin)
